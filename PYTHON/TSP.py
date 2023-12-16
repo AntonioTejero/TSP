68a620be404b01606b4d15f8f0b69b6024535f19
Antonio Tejero Del Caz <antonio.tejero@uco.es>
2023-12-16 20:11:17 +0100

    Added a bunch of features related to the Simulated annealing algorithm

--- PYTHON/TSP.py
+++ PYTHON/TSP.py
@@ -109,6 +109,25 @@ class MainWindow(QtWidgets.QMainWindow):
         stopingCriteriaSimulatedAnnealingSpinBox.setMaximum(10000)
         stopingCriteriaSimulatedAnnealingLayout.addWidget(stopingCriteriaSimulatedAnnealingSpinBox)
         self.stopingCriteriaSimulatedAnnealingSpinBox = stopingCriteriaSimulatedAnnealingSpinBox
+        initialRouteLayout = QtWidgets.QHBoxLayout()
+        solverConfLayoutSimulatedAnnealing.addLayout(initialRouteLayout)
+        initialRouteLabel = QtWidgets.QLabel("Initial route:")
+        initialRouteLayout.addWidget(initialRouteLabel)
+        initialRouteSelectionBox = QtWidgets.QComboBox()
+        initialRouteLayout.addWidget(initialRouteSelectionBox)
+        initialRouteSelectionBox.addItem("Ordered")
+        initialRouteSelectionBox.addItem("Random")
+        self.initialRouteSelectionBox = initialRouteSelectionBox
+        newRouteLayout = QtWidgets.QHBoxLayout()
+        solverConfLayoutSimulatedAnnealing.addLayout(newRouteLayout)
+        newRouteLabel = QtWidgets.QLabel("New routes:")
+        newRouteLayout.addWidget(newRouteLabel)
+        newRouteSelectionBox = QtWidgets.QComboBox()
+        newRouteLayout.addWidget(newRouteSelectionBox)
+        newRouteSelectionBox.addItem("Random")
+        newRouteSelectionBox.addItem("Swap")
+        newRouteSelectionBox.addItem("Inverse")
+        self.newRouteSelectionBox = newRouteSelectionBox
         
         self.solverConfLayout = solverConfLayout
         solverConfLayoutSimulatedAnnealing.addStretch()
@@ -180,6 +199,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.solutionsClosestNeighbourRoute = []
         self.solutionsClosestNeighbourDistance = []
         self.solutionsClosestNeighbourExecutionTime = []
+        self.solutionsSimulatedAnnealingRoute = []
+        self.solutionsSimulatedAnnealingDistance = []
+        self.solutionsSimulatedAnnealingExecutionTime = []
         
     def generateButtonClicked(self):
         
@@ -217,11 +239,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.solutionsClosestNeighbourRoute = []
         self.solutionsClosestNeighbourDistance = []
         self.solutionsClosestNeighbourExecutionTime = []
+        self.solutionsSimulatedAnnealingRoute = []
+        self.solutionsSimulatedAnnealingDistance = []
+        self.solutionsSimulatedAnnealingExecutionTime = []
         self.solutionSelectionBox.clear()
         self.solutionSelectionBox.setEnabled(False)
         self.solutionRouteLabel.setText("Route: ")
         self.solutionDistanceLabel.setText("Distance: ")
         self.solutionExecutionTimeLabel.setText("Exec. time (s): ")
+            
+        for i in range(self.initialRouteSelectionBox.count()):
+            if self.initialRouteSelectionBox.itemText(i) == "Closest neighbour":
+                self.initialRouteSelectionBox.removeItem(i)
             
         self.numberOfCities = numberOfCities
         self.cityXpos = cityXpos
@@ -361,6 +390,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.solutionDistanceLabel.setText("Distance: " + str(bestRouteDistance))
         self.solutionExecutionTimeLabel.setText("Exec. time (s): " + str(finishTime-startTime))
         
+        self.initialRouteSelectionBox.addItem("Closest neighbour")
+        
         self.numberOfCitiesSpinBox.setEnabled(True)
         self.generateButton.setEnabled(True)
         self.algorithmSelectionBox.setEnabled(True)
@@ -376,16 +407,31 @@ class MainWindow(QtWidgets.QMainWindow):
         self.initialTemperatureSpinBox.setEnabled(False)
         self.coolingRateSpinBox.setEnabled(False)
         self.stopingCriteriaSimulatedAnnealingSpinBox.setEnabled(False)
+        self.initialRouteSelectionBox.setEnabled(False)
+        self.newRouteSelectionBox.setEnabled(False)
         
         startTime = time.time()
         
         temperature = self.initialTemperatureSpinBox.value()
         coolingRate = self.coolingRateSpinBox.value()
         
-        # generate initial route randomly
-        auxArray = np.random.uniform(0,1,self.numberOfCities)
-        oldRoute = list(np.argsort(auxArray))
-        oldRoute.append(oldRoute[0])
+        # generate initial route according to user solutionSelectionBox
+        if self.initialRouteSelectionBox.currentText() == "Ordered":
+            # generate ordered sequence from 0 to numberOfCities-1
+            oldRoute = list(range(self.numberOfCities))
+            # append initial city to the route (circular route)
+            oldRoute.append(oldRoute[0])
+        elif self.initialRouteSelectionBox.currentText() == "Random":
+            # generate initial route randomly
+            auxArray = np.random.uniform(0,1,self.numberOfCities)
+            oldRoute = list(np.argsort(auxArray))
+            # append initial city to the route (circular route)
+            oldRoute.append(oldRoute[0])
+        elif self.initialRouteSelectionBox.currentText() == "Closest neighbour":
+            # select route obtained in the Closest neighbour algorithm
+            oldRoute = self.solutionsClosestNeighbourRoute
+            
+        # evaluate initial route total distance
         oldRouteTotalDistance = 0
         for idx in range(self.numberOfCities):
             i = oldRoute[idx]
@@ -400,34 +446,45 @@ class MainWindow(QtWidgets.QMainWindow):
         iterationArray = [0]
         totalDistanceArray = [oldRouteTotalDistance]
         while iterationsWithoutUpdate < maxIterationsWithoutUpdate:
-            # random new route
-            #auxArray = np.random.uniform(0,1,self.numberOfCities)
-            #newRoute = list(np.argsort(auxArray))
-            #newRoute.append(newRoute[0])
-            
-            # generate random indices for swap and inverse operators
-            i = np.random.choice(range(self.numberOfCities-1))
-            while i==0:
+            # generate new route
+            if self.newRouteSelectionBox.currentText() == "Random":
+                # random new route
+                auxArray = np.random.uniform(0,1,self.numberOfCities)
+                newRoute = list(np.argsort(auxArray))
+                newRoute.append(newRoute[0])
+            elif self.newRouteSelectionBox.currentText() == "Swap":
+                # generate random indices for swap and inverse operators
                 i = np.random.choice(range(self.numberOfCities-1))
-            j = np.random.choice(range(self.numberOfCities-1))
-            while j==i or j==0:
+                while i==0:
+                    i = np.random.choice(range(self.numberOfCities))
                 j = np.random.choice(range(self.numberOfCities-1))
-            newRoute = copy.copy(oldRoute)
-            # swap new route
-            #newRoute[j], newRoute[i] = newRoute[i], newRoute[j]
-            # inverse new route
-            newRoute[min(i,j):max(i,j)] = newRoute[max(i,j)-1:min(i,j)-1:-1]
+                while j==i or j==0:
+                    j = np.random.choice(range(self.numberOfCities))
+                # swap new route
+                newRoute = copy.copy(oldRoute)
+                newRoute[j], newRoute[i] = newRoute[i], newRoute[j]
+            elif self.newRouteSelectionBox.currentText() == "Inverse":
+                # generate random indices for swap and inverse operators
+                i = np.random.choice(range(self.numberOfCities-1))
+                while i==0:
+                    i = np.random.choice(range(self.numberOfCities))
+                j = np.random.choice(range(self.numberOfCities-1))
+                while j==i or j==0:
+                    j = np.random.choice(range(self.numberOfCities))
+                # inverse new route
+                newRoute = copy.copy(oldRoute)
+                newRoute[min(i,j):max(i,j)+1] = newRoute[max(i,j):min(i,j)-1:-1]
             
             # evaluate new route total distance
             newRouteTotalDistance = 0
             for idx in range(self.numberOfCities):
-                i = newRoute[idx]
-                j = newRoute[idx+1]
-                newRouteTotalDistance = newRouteTotalDistance + self.distance[i][j]
+                initialCity = newRoute[idx]
+                finalCity = newRoute[idx+1]
+                newRouteTotalDistance = newRouteTotalDistance + self.distance[initialCity][finalCity]
                 
             # decide wheter the new route gets selected or not
             if newRouteTotalDistance <= oldRouteTotalDistance:
-                oldRoute = newRoute
+                oldRoute = copy.copy(newRoute)
                 oldRouteTotalDistance = newRouteTotalDistance
                 temperature = temperature*coolingRate
                 if self.liveSolverCheckBox.isChecked():
@@ -437,7 +494,7 @@ class MainWindow(QtWidgets.QMainWindow):
                     QtCore.QCoreApplication.processEvents()
                 iterationsWithoutUpdate = 0
             elif np.random.uniform(0,1,1)<np.exp(-(newRouteTotalDistance-oldRouteTotalDistance)/temperature):
-                oldRoute = newRoute
+                oldRoute = copy.copy(newRoute)
                 oldRouteTotalDistance = newRouteTotalDistance
                 temperature = temperature*coolingRate
                 if self.liveSolverCheckBox.isChecked():
@@ -456,20 +513,35 @@ class MainWindow(QtWidgets.QMainWindow):
             if self.liveSolverCheckBox.isChecked():
                 self.convergenceCurve.set_xdata(iterationArray)
                 self.convergenceCurve.set_ydata(totalDistanceArray)
-                self.convergenceAxes.set(xlim=(0,iterationArray[-1]), ylim=(0,np.amax(totalDistanceArray)))
+                self.convergenceAxes.set(xlim=(1,iterationArray[-1]), ylim=(0,np.amax(totalDistanceArray)))
                 self.convergenceAxes.figure.canvas.draw()
                 QtCore.QCoreApplication.processEvents()
                 
-            print(temperature)
+            print(iterationsWithoutUpdate, temperature)
             
+        
+        self.mapOfCitiesRoute.set_xdata(self.cityXpos[oldRoute])
+        self.mapOfCitiesRoute.set_ydata(self.cityYpos[oldRoute])
+        self.mapOfCitiesScatter.figure.canvas.draw()
         self.convergenceCurve.set_xdata(iterationArray)
         self.convergenceCurve.set_ydata(totalDistanceArray)
-        self.convergenceAxes.set(xlim=(0,iterationArray[-1]), ylim=(0,np.amax(totalDistanceArray)))
+        self.convergenceAxes.set(xlim=(1,iterationArray[-1]), ylim=(0,np.amax(totalDistanceArray)))
         self.convergenceAxes.figure.canvas.draw()
         QtCore.QCoreApplication.processEvents()
         print("Best route: ", oldRoute, "(", oldRouteTotalDistance, ")")
 
         finishTime = time.time()
+        
+        self.solutionsSimulatedAnnealingRoute = oldRoute
+        self.solutionsSimulatedAnnealingDistance = oldRouteTotalDistance
+        self.solutionsSimulatedAnnealingExecutionTime = finishTime-startTime
+        for i in range(self.solutionSelectionBox.count()):
+            if self.solutionSelectionBox.itemText(i) == "Simulated annealing":
+                self.solutionSelectionBox.removeItem(i)
+        self.solutionSelectionBox.addItem("Simulated annealing")
+        self.solutionRouteLabel.setText("Route: " + str([i+1 for i in oldRoute]))
+        self.solutionDistanceLabel.setText("Distance: " + str(oldRouteTotalDistance))
+        self.solutionExecutionTimeLabel.setText("Exec. time (s): " + str(finishTime-startTime))
         
         self.numberOfCitiesSpinBox.setEnabled(True)
         self.generateButton.setEnabled(True)
@@ -479,6 +551,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.initialTemperatureSpinBox.setEnabled(True)
         self.coolingRateSpinBox.setEnabled(True)
         self.stopingCriteriaSimulatedAnnealingSpinBox.setEnabled(True)
+        self.initialRouteSelectionBox.setEnabled(True)
+        self.newRouteSelectionBox.setEnabled(True)
         
     def solutionSelectedChanged(self):
         
@@ -490,6 +564,10 @@ class MainWindow(QtWidgets.QMainWindow):
             route = self.solutionsClosestNeighbourRoute
             distance = self.solutionsClosestNeighbourDistance
             executionTime = self.solutionsClosestNeighbourExecutionTime
+        elif self.solutionSelectionBox.currentText() == "Simulated annealing":
+            route = self.solutionsSimulatedAnnealingRoute
+            distance = self.solutionsSimulatedAnnealingDistance
+            executionTime = self.solutionsSimulatedAnnealingExecutionTime
         self.solutionRouteLabel.setText("Route: " + str([i+1 for i in route]))
         self.solutionDistanceLabel.setText("Distance: " + str(distance))
         self.solutionExecutionTimeLabel.setText("Exec. Time (s): " + str(executionTime))
